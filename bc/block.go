18445f4b6c49270a1d4025bd31f8d8eb3ea9b7d6
vick_glb <vick_glb@me.com>
2019-09-11 16:01:45 +0800

    block 结构体完善

--- bc/block.go
+++ bc/block.go
@@ -3,9 +3,13 @@ package bc
 import "crypto/sha256"
 
 type Block struct {
-	PrivHash []byte
-	Hash     []byte
-	Data     []byte
+	Version    uint64
+	PrivHash   []byte
+	MerkelRoot []byte
+	TimeStamp  uint64
+	Difficulty uint64
+	Hash       []byte
+	Data       []byte
 }
 
 func NewBlock(data string, prevBlockHash []byte) (block *Block) {
