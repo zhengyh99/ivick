969fc0dbe002e30134e0142a78b592e2b47cd53b
vick_glb <vick_glb@me.com>
2019-09-15 22:45:07 +0800

    add proof or work; modify block.go blockChain.go

--- bc/block.go
+++ bc/block.go
@@ -1,32 +1,78 @@
 package bc
 
-import "crypto/sha256"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"math/big"
+	"time"
+)
 
+//定义块
 type Block struct {
 	Version    uint64
 	PrivHash   []byte
 	MerkelRoot []byte
 	TimeStamp  uint64
 	Difficulty uint64
+	Nonce      uint64
 	Hash       []byte
 	Data       []byte
 }
 
+//创建块
 func NewBlock(data string, prevBlockHash []byte) (block *Block) {
+	var now time.Time
 	block = &Block{
-		PrivHash: prevBlockHash,
-		Hash:     []byte{},
-		Data:     []byte(data),
+		Version:    0,
+		PrivHash:   prevBlockHash,
+		MerkelRoot: []byte{},
+		TimeStamp:  uint64(now.Unix()),
+		Difficulty: 0,
+		Hash:       []byte{},
+		Data:       []byte(data),
 	}
-	block.setHash()
+	pow := NewProofOfWork(block)
+	hash, nonce := pow.Run()
+	block.Hash = hash
+	block.Nonce = nonce
 	return
 }
 
-func (block *Block) setHash() {
-	blockInfo := append(block.PrivHash, block.Data...)
-	hash := sha256.Sum256(blockInfo)
-	block.Hash = hash[:]
+//uint64类型转换成[]byte
+func (block *Block) uintToBytes(num uint64) []byte {
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, num)
+	if err != nil {
+		fmt.Println("binary write error :", err)
+	}
+	return buffer.Bytes()
+
 }
+
+//计算区块hash值和工作量目标值 ，nonce为难度值
+func (block *Block) GetHashAndTarget(nonce uint64) ([]byte, big.Int) {
+
+	tmp := [][]byte{
+		block.uintToBytes(block.Version),
+		block.PrivHash,
+		block.MerkelRoot,
+		block.uintToBytes(block.TimeStamp),
+		block.uintToBytes(block.Difficulty),
+		block.uintToBytes(nonce),
+		block.Hash,
+		block.Data,
+	}
+	blockInfo := bytes.Join(tmp, []byte{})
+	hash := sha256.Sum256(blockInfo)
+	tmpInt := big.Int{}
+	tmpInt.SetBytes(hash[:])
+	return hash[:], tmpInt
+}
+
+//生成创世块
 func GenesisBlock() *Block {
-	return NewBlock("创世块", []byte{})
+	hash := sha256.Sum256([]byte("创世块"))
+	return NewBlock("创世块", hash[:])
 }
--- bc/blockChain.go
+++ bc/blockChain.go
@@ -1,9 +1,11 @@
 package bc
 
+//定义区块链
 type BlockChain struct {
 	Blocks []*Block
 }
 
+//创建区块链
 func NewBlockChain() *BlockChain {
 	genesisBlock := GenesisBlock()
 	return &BlockChain{
@@ -11,6 +13,7 @@ func NewBlockChain() *BlockChain {
 	}
 }
 
+//向区块链中加入区块
 func (bc *BlockChain) AddBlock(data string) {
 	lastBlock := bc.Blocks[len(bc.Blocks)-1]
 	block := NewBlock(data, lastBlock.Hash)
