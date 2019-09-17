c12a8929a92ca6281444b5b73265b8962c5a5413
vick_glb <vick_glb@me.com>
2019-09-17 16:05:33 +0800

    新建boltUse/db.go用于对bolt本地数据库的基本操作

---
+++ boltUse/db.go
@@ -0,0 +1,91 @@
+package boltUse
+
+import (
+	"bolt"
+	"fmt"
+)
+
+//定义结构体
+type BoltDB struct {
+	db     *bolt.DB //数据库
+	bucket []byte   //桶
+}
+
+//打开数据库文件
+func OpenBoltDB(fileName string) *BoltDB {
+	boltdb, err := bolt.Open(fileName, 0600, nil)
+	if err != nil {
+		fmt.Println("bolt open error:", err)
+	}
+	MyDB := &BoltDB{
+		db: boltdb,
+	}
+	MyDB.SetBucket("") //初始化桶
+	return MyDB
+}
+
+//关闭数据库
+func (this *BoltDB) Close() {
+	this.db.Close()
+}
+
+//设置当前桶
+func (this *BoltDB) SetBucket(bucketName string) error {
+	if bucketName == "" {
+		bucketName = "default"
+	}
+	bucket := []byte(bucketName)
+	if err := this.db.Update(func(tx *bolt.Tx) error {
+		if _, err := tx.CreateBucketIfNotExists(bucket); err != nil { //判断桶是否存在，不存在建新
+			fmt.Println("create failed", err.Error())
+			return err
+		}
+		return nil
+	}); err != nil {
+		fmt.Println("update error is:", err)
+	}
+	this.bucket = bucket
+	return nil
+
+}
+
+//添加/修改新的键值
+func (this *BoltDB) Put(key, value []byte) error {
+	if err := this.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(this.bucket)
+		err := b.Put(key, value) //添加 健值
+		return err
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+//依键得值
+func (this *BoltDB) GET(key []byte) (value []byte) {
+	if err := this.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(this.bucket)
+		value = b.Get(key) //依健查值
+		return nil
+	}); err != nil {
+		fmt.Println("view error :", err.Error())
+		return []byte("")
+	}
+	return
+}
+
+//查询指定桶中的所有键值
+func (this *BoltDB) GetAll(bucketName string) (datas []interface{}) {
+	this.SetBucket(bucketName)
+	this.db.View(func(tx *bolt.Tx) error {
+		// Assume bucket exists and has keys
+		b := tx.Bucket(this.bucket)
+		b.ForEach(func(k, v []byte) error { //遍历
+			kv := [][]byte{k, v}
+			datas = append(datas, kv)
+			return nil
+		})
+		return nil
+	})
+	return
+}
--- main.go
+++ main.go
@@ -2,18 +2,39 @@ package main
 
 import (
 	"fmt"
-	"zoin/bc"
+	_ "fmt"
+	"zoin/boltUse"
 )
 
 func main() {
-	blockChain := bc.NewBlockChain()
-	blockChain.AddBlock("block1")
-	blockChain.AddBlock("block2")
-	for i, block := range blockChain.Blocks {
-		fmt.Printf("=====当前区块高度 %d\n", i)
-		fmt.Printf("block prevhash: %x \n", block.PrivHash)
-		fmt.Printf("block hash: %x \n", block.Hash)
-		fmt.Printf("block data %s \n", block.Data)
+	// blockChain := bc.NewBlockChain()
+	// blockChain.AddBlock("block1")
+	// blockChain.AddBlock("block2")
+	// for i, block := range blockChain.Blocks {
+	// 	fmt.Printf("=====当前区块高度 %d\n", i)
+	// 	fmt.Printf("block prevhash: %x \n", block.PrivHash)
+	// 	fmt.Printf("block hash: %x \n", block.Hash)
+	// 	fmt.Printf("block data %s \n", block.Data)
+	// }
+	db := boltUse.OpenBoltDB("testDB.db")
+	//添加数据
+	if err := db.Put([]byte("NiHao"), []byte("你好")); err != nil {
+		fmt.Println("db put error:", err)
 	}
+	if err := db.Put([]byte("shijie"), []byte("世界")); err != nil {
+		fmt.Println("db put error:", err)
+	}
+	//依键得值
+	v := db.GET([]byte("NiHao"))
+	fmt.Printf("Key:nihao 's value:%s\n", v)
+	//遍历默认桶中的键值对
+	datas := db.GetAll("")
+	for k, v := range datas {
+		if value, ok := v.([][]byte); ok { //数据断言
+			fmt.Printf("第%v个数据：, [%s] is %s\n", k, value[0], value[1])
+		}
+
+	}
+	db.Close()
 
 }
